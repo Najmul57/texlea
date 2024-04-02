cbdfb584b92258cc52752134757e72d786c684c3
najmul57 <najmulcse2@gmail.com>
2024-04-02 16:42:16 +0600

    home portfolio design done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 (function ($) {
     "use strict";
 
+    //category toggle
+    var categoryToggles = document.querySelectorAll('.categoryToggle');
+
+    categoryToggles.forEach(function (toggle) {
+
+        toggle.addEventListener('click', function (event) {
+
+            event.preventDefault();
+
+            var categoryList = toggle.nextElementSibling;
+
+            if (categoryList.style.display === 'none' || categoryList.style.display === '') {
+                categoryList.style.display = 'block';
+            } else {
+                categoryList.style.display = 'none';
+            }
+
+            var subcategoryLists = categoryList.querySelectorAll('.mobile__subcategory');
+
+            subcategoryLists.forEach(function (subcategoryList) {
+
+                if (subcategoryList.style.display === 'none' || subcategoryList.style.display === '') {
+                    subcategoryList.style.display = 'block';
+                } else {
+                    subcategoryList.style.display = 'none';
+                }
+            });
+        });
+    });
+
     // home slider
     $('.slider__active').slick({
         dots: true,
@@ -8,35 +38,77 @@
         slidesToScroll: 1,
         infinite: true,
         speed: 500,
-        arrows:false,
+        arrows: false,
         autoplay: true,
         fade: true,
         cssEase: 'linear'
     });
 
-    // scroll to top button 
-document.addEventListener("DOMContentLoaded", function() {
+    // testimonial-active
+    $('.certificate-active').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: 'linear',
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        swipe: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 
-    let scrollTOTopBtn = document.querySelector('.scroll-top')
-  
-    window.addEventListener("scroll", function() {
-      // Show or hide the button based on the scroll position
-      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollTOTopBtn.classList.add("open");
-      } else {
-        scrollTOTopBtn.classList.remove("open");
-      }
+
+    // scroll to top button 
+    document.addEventListener("DOMContentLoaded", function () {
+
+        let scrollTOTopBtn = document.querySelector('.scroll-top')
+
+        window.addEventListener("scroll", function () {
+            // Show or hide the button based on the scroll position
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                scrollTOTopBtn.classList.add("open");
+            } else {
+                scrollTOTopBtn.classList.remove("open");
+            }
+        });
+
+        scrollToTopBtn.addEventListener("click", function () {
+            // Scroll to the top with smooth animation
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        });
     });
-  
-    scrollToTopBtn.addEventListener("click", function() {
-      // Scroll to the top with smooth animation
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    });
-  });
-  
-    
-    
+
+
+
 
 
 })(jQuery);

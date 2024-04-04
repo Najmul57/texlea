3b876bd626a1c83ef4ef8fe7056cfdc3091a6703
najmul57 <najmulcse2@gmail.com>
2024-04-04 10:52:38 +0600

    category and office page done

--- js/main.js
+++ js/main.js
@@ -117,6 +117,33 @@
         });
     });
 
+    // office 
+    $('.office__active').slick({
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 5,
+        autoplay:true,
+        arrows:false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+          
 
 
 })(jQuery);
